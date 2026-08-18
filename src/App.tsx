fe14f72624aec613e992c74871ca6de55c47dc93
Navyanth Kumar Reddy Bollareddy <navyanth1234@gmail.com>
2026-08-17 22:16:19 -0400

    Add StatsBar component and wire it into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { useState } from 'react'
-// import { Vehicle, VehicleStatus} from './types/vehicle'
+import type { Vehicle, VehicleStatus} from './types/vehicle'
 import {mockVehicles} from './data/mockVehicles'
 
 import SearchBar from './components/SearchBar'
+import StatusFilter from './components/StatusFilter'
+import StatsBar from './components/StatsBar'
 
 function App() {
-  // const [vehicles, setVehicles] = useState<Vehicle[]>(mockVehicles)
+  const [vehicles, setVehicles] = useState<Vehicle[]>(mockVehicles)
 
   const [searchTerm, setSearchTerm] = useState<string>('')
 
-  // const [statusFilter,setStatusFilter] = useState<VehicleStatus | 'all'>('all')
+  const [statusFilter,setStatusFilter] = useState<VehicleStatus | 'all'>('all')
 
   // const [selectedVehicle, setSelectedVehicle] = useState<Vehicle | null>(null)
 
   return (
     <>
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <StatusFilter statusFilter={statusFilter} setStatusFilter={setStatusFilter} />
+      <StatsBar vehicles = {vehicles}/>
       
     </>
   )
